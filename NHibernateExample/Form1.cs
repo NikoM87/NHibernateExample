3dc998385e54b43dec67e8479c0251458c222c5a
Nikolai Melkov <nikom87@gmail.com>
2015-09-28 02:30:44 +0500

    SQLite

---
+++ NHibernateExample/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using Environment = System.Environment;
+
+namespace NHibirnateExample
+{
+    public partial class Form1 : Form
+    {
+        private ISession _sess;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof (Product).Assembly);
+
+            var sessions = cfg.BuildSessionFactory();
+            _sess = sessions.OpenSession();
+
+            new SchemaExport(cfg).Create(true, true);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+
+            var product = new Product
+            {
+                Name = "Minesweeper",
+                Price = 300,
+                Category = "Game"
+            };
+            _sess.Save(product);
+            _sess.Flush();
+
+            var q = _sess.CreateQuery("FROM Product");
+            var list = q.List<Product>();
+
+            foreach (var p in list.ToList())
+                richTextBox1.Text += p + Environment.NewLine;
+        }
+    }
+}
---
+++ NHibernateExample/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NHibirnateExample
+{
+    public class Product
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Category { get; set; }
+        public virtual int Price { get; set; }
+
+        public override string ToString()
+        {
+            return "Id: " + Id + Environment.NewLine +
+                   "Name: " + Name + Environment.NewLine +
+                   "Category: " + Category + Environment.NewLine +
+                   "Price: " + Price + Environment.NewLine;
+        }
+    }
+}
